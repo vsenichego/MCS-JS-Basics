1fec2f2bf99ce78398e3201410420d15bd58bee2
vsenichego <andreygovorunn@gmail.com>
2021-04-22 20:14:00 +0300

    Update script.js

--- practice_4.8.1/js/script.js
+++ practice_4.8.1/js/script.js
@@ -1,11 +1,25 @@
+// let haveEnough = function(allCash, allWatches, allEarrings, costWatches, costEarrings) {
+//     allCash = parseInt(prompt('enter your cash'));
+//     allWatches = parseInt(prompt('enter your watches'));
+//     allEarrings = parseInt(prompt('enter your bryuliks'));
+//     costWatches = parseInt(prompt('enter price of watches')) * allWatches;
+//     costEarrings = parseInt(prompt('enter price of bryuliks')) * allEarrings;
+//     allPrice = costWatches + costEarrings;
+//     (allCash >= allPrice) ? (document.body.innerHTML = 'Enough $$$'): document.body.innerHTML = 'Not enough $$$';
+// }
+
+// haveEnough();
+
+
 let haveEnough = function(allCash, allWatches, allEarrings, costWatches, costEarrings) {
-    allCash = parseInt(prompt('enter your cash'));
-    allWatches = parseInt(prompt('enter your watches'));
-    allEarrings = parseInt(prompt('enter your bryuliks'));
-    costWatches = parseInt(prompt('enter price of watches')) * allWatches;
-    costEarrings = parseInt(prompt('enter price of bryuliks')) * allEarrings;
-    allPrice = costWatches + costEarrings;
-    (allCash >= allPrice) ? (document.body.innerHTML = 'Enough $$$'): document.body.innerHTML = 'Not enough $$$';
+		let allPrice = costWatches + costEarrings;
+		return (allCash >= allPrice) ? (document.body.innerHTML = 'Enough $$$'): (document.body.innerHTML = 'Not enough $$$');
 }
 
-haveEnough();
+const allCash = parseInt(prompt('enter your cash'));
+const allWatches = parseInt(prompt('enter your watches'));
+const allEarrings = parseInt(prompt('enter your bryuliks'));
+const costWatches = parseInt(prompt('enter price of watches')) * allWatches;
+const costEarrings = parseInt(prompt('enter price of bryuliks')) * allEarrings;
+
+document.body.innerHTML = haveEnough(allCash, allWatches, allEarrings, costWatches, costEarrings);
