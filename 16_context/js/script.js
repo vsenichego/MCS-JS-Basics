049e899d3948e6f89d8a6381facf6c2d8ac65c9e
vsenichego <andreygovorunn@gmail.com>
2021-04-23 19:47:16 +0300

    lessons

---
+++ 16_context/js/script.js
@@ -0,0 +1,14 @@
+console.log(this);
+
+window.onscroll = function(){console.log(this);}
+
+const button = document.querySelector('button');
+
+button.onclick = function(){
+	this.innerHTML = 'Pressed';
+
+}
+
+button.onclick = () =>{
+	console.log(this);
+}
