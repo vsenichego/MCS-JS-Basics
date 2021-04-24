baab4172e3b841c51cb9c893e3f260e236f7f5d8
vsenichego <andreygovorunn@gmail.com>
2021-04-24 18:49:18 +0300

    made lesson and practice
    from chapter "Complex data"

---
+++ 18_math/js/script.js
@@ -0,0 +1,14 @@
+function moduleSquare(num) {
+	return Math.abs(num)/2;
+}
+
+console.log(moduleSquare(-3));
+console.log(moduleSquare(3));
+
+console.log(Math.floor(2.996));
+console.log(Math.round(2.789));
+
+console.log(Math.random());
+
+console.log(Math.random() * (10 - 5) + 5);
+console.log(Math.floor(Math.random() * (10 - 5) + 5));
