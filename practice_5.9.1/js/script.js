68b1efdfa747d259f60e1e20443a9599e0f8568b
Andrew <andreygovorunn@gmail.com>
2021-04-29 19:59:38 +0300

    Update script.js

--- practice_5.9.1/js/script.js
+++ practice_5.9.1/js/script.js
@@ -1,9 +1,17 @@
-let i = 0;
-while(i < 100000) {
+// let i = 0;
+// while(i < 100000) {
+//     if (i%6==0){
+//         let div = document.createElement('div');
+//         div.innerHTML = i;
+//         document.body.append(div);
+//     }
+//     i++;
+// }
+
+for(let i=0; i<100000; i++) {
     if (i%6==0){
         let div = document.createElement('div');
         div.innerHTML = i;
         document.body.append(div);
-    }
-    i++;
+    };
 }
