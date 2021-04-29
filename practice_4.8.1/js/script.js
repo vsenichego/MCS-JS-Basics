2eed9d64c82e8d848ebb7a8d0430c1a4e9d641ad
Andrew <andreygovorunn@gmail.com>
2021-04-29 17:05:48 +0300

    Update script.js

--- practice_4.8.1/js/script.js
+++ practice_4.8.1/js/script.js
@@ -1,16 +1,3 @@
-// let haveEnough = function(allCash, allWatches, allEarrings, costWatches, costEarrings) {
-//     allCash = parseInt(prompt('enter your cash'));
-//     allWatches = parseInt(prompt('enter your watches'));
-//     allEarrings = parseInt(prompt('enter your bryuliks'));
-//     costWatches = parseInt(prompt('enter price of watches')) * allWatches;
-//     costEarrings = parseInt(prompt('enter price of bryuliks')) * allEarrings;
-//     allPrice = costWatches + costEarrings;
-//     (allCash >= allPrice) ? (document.body.innerHTML = 'Enough $$$'): document.body.innerHTML = 'Not enough $$$';
-// }
-
-// haveEnough();
-
-
 let haveEnough = function(allCash, allWatches, allEarrings, costWatches, costEarrings) {
 		let allPrice = costWatches + costEarrings;
 		return (allCash >= allPrice) ? (document.body.innerHTML = 'Enough $$$'): (document.body.innerHTML = 'Not enough $$$');
