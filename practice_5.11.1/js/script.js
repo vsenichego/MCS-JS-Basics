eaa361fcdfeda99de207a45a5325bb508480e2ad
vsenichego <andreygovorunn@gmail.com>
2021-04-30 15:28:01 +0300

    Update script.js

--- practice_5.11.1/js/script.js
+++ practice_5.11.1/js/script.js
@@ -1,4 +1,4 @@
-let newSchool = [
+let newSchools = [
   [
     ['Lil Yachty', 'Playboi Carti', 'Lil Uzi Vert'],
     ['Sonny Digital', 'Metro Boomin', '12Hunna']
@@ -7,27 +7,12 @@ let newSchool = [
     ['Yung Lean', 'Ecco2K', 'Bladee'],
     ['Whitearmor', 'PJ Beats', 'Yung Gud']
   ]
-];
+]
 
-// console.log(newSchool[0][0][0]);
-
-// let i = 0
-// let x = 0;
-// let y = 0;
-// while(i < 2) {
-//     while(x < 3 && y < 3) {
-//         console.log(newSchool[i][x][y]);
-//         y++;
-//         }
-//     x++;
-//     i++;
-// }
-
-for(newS of newSchool) {
-            console.log(newS); 
-    for(let i = 0; i < length; i++) {
-        alert(i);
-        console.log(newS[i]); 
+for(let newSchool of newSchools) {
+  for (let element of newSchool) {
+    for(let text of element) {
+          console.log(text);
     }
-
+  }
 }
