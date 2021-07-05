37ee2a3ff1875b00c9515b27895a816f305ec3b3
vsenichego <andreygovorunn@gmail.com>
2021-07-05 21:29:17 +0300

    doing practice 5

---
+++ practice_5/js/script.js
@@ -0,0 +1,14 @@
+let crypto = [
+    {
+      name: "Bitcoin",
+      price: 1388.37
+    },
+    {
+      name: "Ethereum",
+      price: 716
+    },
+    {
+      name: "Litecoin",
+      price: 140
+    }
+  ]
